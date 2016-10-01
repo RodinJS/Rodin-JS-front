361097f15b3c6cfd4ff62c315e1d29e5358efa07
gormat <gormatevosyan14@gmail.com>
2016-10-01 20:23:43 +0400

    update

--- src/app/helpers.js
+++ src/app/helpers.js
@@ -3,7 +3,7 @@ Object.filterByKeys = function(obj, keys) {
 
     for(let i = 0; i < keys.length; i ++)  {
         const key = keys[i];
-        if(obj[key]) {
+        if(obj.hasOwnProperty(key)) {
             ret[key] = obj[key];
         }
     }
