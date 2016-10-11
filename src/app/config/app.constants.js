a71e98c006778fa7bbc1444a4a8047d7db7f9013
gormat <gormatevosyan14@gmail.com>
2016-10-11 16:58:24 +0400

    update

--- src/app/config/app.constants.js
+++ src/app/config/app.constants.js
@@ -4,16 +4,16 @@
 import '../helpers';
 
 const AppConstants = {
-	env: "prod",
+	env: "dev",
 	jwtKey: 'token',
 	appName: 'Rodin',
 
 	dev: {
 		COOKIEDOMAIN: ['localhost', '.rodin.space'],
-		// API: 'http://localhost:3000/api',
-		API: 'https://api.rodinapp.com/api',
-		// SITE: 'http://localhost:3000/api',
-		SITE: 'http://localhost:8585/#/',
+		API: 'http://localhost:3000/api',
+		// API: 'https://api.rodinapp.com/api',
+		SITE: 'http://localhost:3000//api',
+		// SITE: 'http://localhost:8585/#/',
 		PREVIEW: 'https://api.rodinapp.com/preview/',
 		PUBLIC: 'https://api.rodinapp.com/public/',
 		EDITOR: 'http://localhost:8000/#/',
