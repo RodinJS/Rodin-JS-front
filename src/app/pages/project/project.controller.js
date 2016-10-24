d531f84871fb72b4568978870bb1668213ecb8a3
gormat <gormatevosyan14@gmail.com>
2016-10-24 21:37:32 +0400

    update

--- src/app/pages/project/project.controller.js
+++ src/app/pages/project/project.controller.js
@@ -1,50 +1,53 @@
 class ProjectCtrl {
-	constructor(AppConstants, Project, $state, $scope, User) {
-		'ngInject';
+    constructor (AppConstants, Project, $state, $scope, User) {
+        'ngInject';
 
-		this.appName = AppConstants.appName;
-		this.editorUrl = AppConstants.EDITOR;
-		this.previewUrl = AppConstants.PREVIEW;
-		this.Project = Project;
-		this.$state = $state;
-		this.currentUser = User.current;
+        this.appName = AppConstants.appName;
+        this.editorUrl = AppConstants.EDITOR;
+        this.previewUrl = AppConstants.PREVIEW;
+        this.Project = Project;
+        this.$state = $state;
+        this.currentUser = User.current;
 
-		$scope.projectDescription = "Project description.";
+        $scope.projectDescription = "Project description.";
 
-		this.project = {
-			name: "",
-			description: $scope.projectDescription
-		};
+        this.project = {
+            name: "",
+            description: $scope.projectDescription,
+            templateOf: "empty"
+        };
 
-		this.showLoader = false;
+        this.showLoader = false;
 
-		this.tinymceOptions = {
-			menubar: false,
-			statusbar: false,
-			toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist',
-			inline: false,
-			plugins: 'advlist autolink link image lists charmap print preview',
-			theme: "modern"
-		};
-	}
+        this.tinymceOptions = {
+            menubar: false,
+            statusbar: false,
+            toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist',
+            inline: false,
+            plugins: 'advlist autolink link image lists charmap print preview',
+            theme: "modern"
+        };
 
-	save() {
-		this.showLoader = true;
+        this.template = 0;
+    }
 
-		let projectInfo = {};
-		angular.extend(projectInfo, this.project);
-		projectInfo.tags = (projectInfo.tags && projectInfo.tags.split(",")) || [];
+    save () {
+        this.showLoader = true;
 
-		this.Project.create(projectInfo).then(
-			data => {
-				this.showLoader = false;
-				this.$state.go('app.dashboard');
-			},
-			err => {
-				this.showLoader = false;
-			}
-		)
-	}
+        let projectInfo = {};
+        angular.extend(projectInfo, this.project);
+        projectInfo.tags = (projectInfo.tags && projectInfo.tags.split(",")) || [];
+
+        this.Project.create(projectInfo).then(
+            data => {
+                this.showLoader = false;
+                this.$state.go('app.dashboard');
+            },
+            err => {
+                this.showLoader = false;
+            }
+        )
+    }
 }
 
 export default ProjectCtrl;
