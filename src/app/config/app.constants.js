211c7e4d0ab5ffcbad624e3f6aedd50fe183bc9a
khlevon98 <kh.levon98@gmail.com>
2016-10-31 14:50:21 +0400

    fixed conflict

--- src/app/config/app.constants.js
+++ src/app/config/app.constants.js
@@ -10,9 +10,8 @@ const AppConstants = {
 
 	dev: {
 		COOKIEDOMAIN: ['localhost', '.rodin.space'],
-		API: 'http://localhost:3000/api',
-		// API: 'https://api.rodin.space/api',
-		// API: 'http://192.168.0.33:3000/api', // Xcho
+		// API: 'http://localhost:3000/api',
+		API: 'https://api.rodin.space/api',
 		SITE: 'http://localhost:8585/#/',
 		PREVIEW: 'https://api.rodin.space/preview/',
 		PUBLIC: 'https://api.rodin.space/public/',
