b54cbe2b4af2dc56d9cd1b12165f397232abe28c
khlevon98 <kh.levon98@gmail.com>
2016-11-24 15:48:49 +0400

    upgrade vers

--- src/app/config/app.constants.js
+++ src/app/config/app.constants.js
@@ -10,7 +10,7 @@ const AppConstants = {
 
 	dev: {
 		COOKIEDOMAIN: ['localhost', '.rodin.space'],
-		//API: 'http://localhost:3000/api',
+		// API: 'http://localhost:3000/api',
 		API: 'https://api.rodin.space/api',
 		// API: 'http://192.168.0.33:3000/api', // Xcho
 		SITE: 'http://localhost:8585/#/',
