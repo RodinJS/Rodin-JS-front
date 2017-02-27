7d2be7041a6abc4ad5dc7472ccb5ce01d039f0c6
xgharibyan <khachatur.gharibyan@gmail.com>
2017-02-27 15:43:34 +0400

    RO-86 # hide slider on mobiles

--- src/app/pages/home/home.controller.js
+++ src/app/pages/home/home.controller.js
@@ -34,7 +34,7 @@ class HomeCtrl {
 
         this.hoveredIframe = false;
         this.emailFocused = false;
-        this.isMobile = true; //this.checkMobile();
+        this.isMobile = this.checkMobile();
 
         this.patterns = {
             email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
