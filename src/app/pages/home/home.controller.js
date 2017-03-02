c961f2eb9da07f039c6e0971441121f437a54427
xgharibyan <khachatur.gharibyan@gmail.com>
2017-03-02 11:38:05 +0400

    RO-86 # fix vr mode enter issue

--- src/app/pages/home/home.controller.js
+++ src/app/pages/home/home.controller.js
@@ -21,6 +21,8 @@ class HomeCtrl {
         this.iframe = angular.element('#iframe');
         this.requestInProgress = false;
         this.initialIframeWidth = false;
+        this.disablePointer = true;
+        this.hoveredIframe = true;
         this.iframe.height(this.windowHeight);
 
         this.scrollHandler = this.scrollHandler.bind(this);
@@ -33,7 +35,6 @@ class HomeCtrl {
         window.addEventListener('message', this.handleIframeMessage, false);
         document.body.addEventListener('wheel', this.scrollHandler, false);
 
-        this.hoveredIframe = false;
         this.emailFocused = false;
         this.isMobile = this.checkMobile();
 
@@ -62,17 +63,17 @@ class HomeCtrl {
                     hljs.highlightBlock(block);
                 });
 
-                $('.back-home-btn').on('click', () =>  this.outToSlider());
+                $('.back-home-btn').on('click', () => this.outToSlider());
             });
 
         });
 
-        $scope.$on('scrollDown', (event, data)=> this.scrollHandler({ deltaY: 1 }));
+        $scope.$on('scrollDown', (event, data) => this.scrollHandler({ deltaY: 1 }));
     }
 
     notificationBoxTrigger($event, backdropClick) {
         const toggleClass = 'focus';
-        const target = $event.target  || '#emailControl';
+        const target = $event.target || '#emailControl';
         if (!backdropClick) {
             this.emailFocused = true;
             angular.element(target).addClass(toggleClass).closest('.input-group').addClass(toggleClass);
@@ -141,24 +142,27 @@ class HomeCtrl {
     }
 
     handleIframeMessage(e) {
-        const scroller = e.data.scroll;
+
+        if (!_.isUndefined(e.data.scroll))
+            this.hoveredIframe  = e.data.scroll;
+
         const wheeler = e.data.wheel;
 
-        if (typeof scroller === 'boolean')
-            this.hoveredIframe = scroller;
+        if (wheeler && wheeler < 0 && this.hoveredIframe) {
+            const $iframeBox = $('#iframe-box');
+            $iframeBox.css({
+                width: this.initialTransform.w,
+                height: this.initialTransform.h,
+                left: this.initialTransform.x,
+                top: this.initialTransform.y,
+            });
+            $('.slide-number').show();
+            $('.pagination-wrapper').show();
+            $('.btn-next-slide').show();
 
-        //console.log(this.hoveredIframe);
-        //console.log(this.disablePointer);
-        if (typeof wheeler === 'boolean') {
-
-            if (this.hoveredIframe) {
-                //angular.element('body').css({ overflow: 'auto' });
-                this.disablePointer = true;
-            } else {
-                //angular.element('body').css({ overflow: 'hidden' });
-                this.disablePointer = false;
-            }
-
+            LANDING.projectSlider.unlockSwipeToPrev();
+            this.disablePointer = true;
+            angular.element('body').removeAttr('style');
         }
 
         this._$scope.$apply();
@@ -198,8 +202,6 @@ class HomeCtrl {
         if (!this.isMobile) {
             if (isBottom && e.deltaY > 0 && ($iframeBox.innerWidth() < this.windowWidth)) {
 
-                this.disablePointer = true;
-
                 $('body').css({ overflow: 'hidden' });
                 $('.slide-number').hide();
                 $('.pagination-wrapper').hide();
@@ -213,35 +215,7 @@ class HomeCtrl {
 
                 iframe.focus();
                 LANDING.projectSlider.lockSwipeToPrev();
-
-            }
-
-            //Stop parent events works in iframe
-            //console.log(($iframeBox.width() - 100), this.windowWidth);
-            if ($iframeBox.innerWidth() >= this.windowWidth) {
-                angular.element('body').css({ overflow: 'hidden' });
                 this.disablePointer = false;
-
-                //this.hoveredIframe = true;
-
-            }
-
-            if (isBottom && e.deltaY < 0) {
-                $iframeBox.css({
-                    width: this.initialTransform.w,
-                    height: this.initialTransform.h,
-                    left: this.initialTransform.x,
-                    top: this.initialTransform.y,
-                });
-                $('.slide-number').show();
-                $('.pagination-wrapper').show();
-                $('.btn-next-slide').show();
-
-                if (($iframeBox.width() - 100) <= this.initialTransform.w) {
-                    this.disablePointer = true;
-                    LANDING.projectSlider.unlockSwipeToPrev();
-                    angular.element('body').removeAttr('style');
-                }
 
             }
 
@@ -252,7 +226,9 @@ class HomeCtrl {
 
     checkMobile() {
         var check = false;
-        (function (a) {if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;})(navigator.userAgent || navigator.vendor || window.opera);
+        (function (a) {
+            if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+        })(navigator.userAgent || navigator.vendor || window.opera);
         return check;
     }
 
@@ -273,11 +249,11 @@ class HomeCtrl {
     submitSubscribe() {
         if (!this.subscribe.email || !this.subscribe.userType) return;
 
-        if(this.requestInProgress) return;
+        if (this.requestInProgress) return;
         const subscribe = angular.copy(this.subscribe);
         this.requestInProgress = true;
         subscribe.position = subscribe.position.join(', ');
-        this._User.subscribe(subscribe).then((result)=> {
+        this._User.subscribe(subscribe).then((result) => {
             this._Notification.success('You have successfully subscribed to notification list');
             $('#emailControl').removeClass('focus').closest('.input-group').removeClass('focus');
             this.emailFocused = false;
@@ -287,7 +263,7 @@ class HomeCtrl {
                 userType: 'Personal',
                 position: [],
             };
-        }, (err)=> {
+        }, (err) => {
             this.subscribe.email = '';
             _.each(err, (val, key) => {
                 this._Notification.error(val.fieldName);
