79d2c9c8467cd37cc2b850ddeebf8aaf906d4405
xgharibyan <khachatur.gharibyan@gmail.com>
2017-03-03 02:15:19 +0400

    RO-248 # update static page logic set codify directive

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -14,6 +14,7 @@ componentsModule.directive('compile', Utils.Compile);
 componentsModule.directive('limitTo', Utils.limitTo);
 componentsModule.directive('closeModal', Utils.CloseModal);
 componentsModule.directive('ngAutocomplete', Utils.NgAutocomplete);
+componentsModule.directive('codify', ['$timeout', Utils.Codify]);
 
 import EditorNavigation from './editorNavigation/index';
 componentsModule.directive('editorNavigation', EditorNavigation);
