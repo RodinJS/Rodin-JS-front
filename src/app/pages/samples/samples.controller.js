70fa4aa50b2f7743c234e386ae313d2a65c0e815
reinchard <reinchard1488@gmail.com>
2017-03-27 11:10:13 +0400

    RO-498 # Samples page

--- src/app/pages/samples/samples.controller.js
+++ src/app/pages/samples/samples.controller.js
@@ -1,7 +1,36 @@
 class SamplesCtrl {
-    constructor() {
+	constructor($scope, $http, $sce) {
+		'ngInject'
+		this._$scope = $scope;
+		this._$sce = $sce;
+		this._$http = $http;
+		this.samples = {};
+		this.activeLink = '';
+		this.setActiveLink = this.setActiveLink.bind(this);
+		this.init = this.init.bind(this);
+		this.setDefaultStates = this.setDefaultStates.bind(this);
+		this.init();
+	}
 
-    }
+	init() {
+		let iframeContent = angular.element('.samples-iframe');
+		iframeContent.css({ height: window.innerHeight - 91 });
+		this._$http.get('https://examples.rodin.io/list.json')
+			.then(res => {
+				this.samples = res.data;
+				let first = Object.keys(this.samples)[0];
+				this.setDefaultStates(this.samples[first].url, this.samples[first].git);
+			})
+	}
+
+	setActiveLink(key) {
+		return this.setDefaultStates(this.samples[key].url, this.samples[key].git)
+	}
+
+	setDefaultStates(src, git) {
+		this.activeLink = this._$sce.trustAsResourceUrl(src);
+		this.sourceLink = this._$sce.trustAsResourceUrl(git);
+	}
 
 }
 
