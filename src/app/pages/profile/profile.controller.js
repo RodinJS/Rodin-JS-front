9d8c4fa0cac1e41e30424f7664e57a90d2865af8
Khachatur Gharibyan <khachatur.gharibyan@gmail.com>
2017-04-06 17:28:28 +0400

    RO-670 # implement GitHub project url Ui part. now when you wright right GitHub url templates will be disabled.

--- src/app/pages/profile/profile.controller.js
+++ src/app/pages/profile/profile.controller.js
@@ -52,7 +52,7 @@ class ProfileCtrl {
             });
         }
         catch (e) {
-            this.Notification.error({message:`Tracking protection is turned on in private mode. Please turn off or use browser normal mode.`, delay: 7000})
+            this.Notification.warning({message:`Tracking protection is turned on in private mode. Please turn off or use browser normal mode.`, delay: 7000})
         }
 
         try {
@@ -64,7 +64,7 @@ class ProfileCtrl {
             });
         }
         catch (e) {
-            this.Notification.error({message:`Tracking protection is turned on in private mode. Please turn off or use browser normal mode.`, delay: 7000})
+            this.Notification.warning({message:`Tracking protection is turned on in private mode. Please turn off or use browser normal mode.`, delay: 7000})
         }
 
         if ($stateParams.token && $stateParams.id && this.currentUser) {
