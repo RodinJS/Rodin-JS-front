d165475dd065e277ae3bddfaeb1e8869c38d9eda
Raffi Hovhannisyan <reinchard1488@gmail.com>
2017-10-05 12:22:56 +0400

    RO-1578 # update footer section , tags min length

--- src/app/layout/main/footer.component.js
+++ src/app/layout/main/footer.component.js
@@ -1,16 +1,27 @@
 class AppFooterCtrl {
-    constructor(AppConstants, $scope, PagesStore) {
+    constructor(AppConstants, $scope, PagesStore, PagesService) {
         'ngInject';
         this.appName = AppConstants.appName;
         this._PagesStore = PagesStore;
-
+        this._PagesService = PagesService;
         // Get today's date to generate the year
         this.date = new Date();
 
         this._PagesStore.subscribeAndInit($scope, () => {
             this.pagesList = this._PagesStore.getFooterPagesList();
         });
+        this.getPagesList()
+    }
+    getPagesList() {
+        this._PagesService.getList().then(
+            pagesList => {
+                this.pagesList = pagesList[0].values.filter((pages) => pages.putOnFooter);
+            },
 
+            err => {
+                console.log(err);
+            }
+        );
     }
 }
 
